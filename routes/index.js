3bc5d21f3f127ad4600eadeb11843c2f8a2959ba
Gustavo Lemos <46227839+GhostsBR@users.noreply.github.com>
2021-03-26 15:26:35 -0300

    Removido algumas rotas desnecessárias
    Removido algumas rotas sem utilidade.

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,9 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 router.get('/', homeController.index);
-router.get('/painel', (req, res) => {res.redirect('/panel')});
 router.get('/panel', authMiddleware.isLogged, mainController.index);
-router.get('/entrar', (req, res) => {res.redirect('/login')})
 router.get('/login', userController.login);
 router.post('/login', userController.loginAction);
-router.get('/registro', (req, res) => {res.redirect('/register')})
 router.get('/register', userController.register);
 router.post('/register', userController.registerAction);
 router.get('/logout', userController.logoutAction);
