9db7aea5bb039f31c5549d788322cdc2b038435d
Gustavo Lemos <46227839+GhostsBR@users.noreply.github.com>
2021-03-26 15:24:54 -0300

    Adicionado nova rota
    Nova rota para adicionar eventos ao calendário.

---
+++ controllers/eventController.js
@@ -0,0 +1,3 @@
+exports.addEvent = (req, res) => {
+
+}
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const homeController = require('../controllers/homeController');
 const mainController = require('../controllers/mainController');
 const userController = require('../controllers/userController');
+const eventController = require('../controllers/eventController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
@@ -16,5 +17,6 @@ router.get('/register', userController.register);
 router.post('/register', userController.registerAction);
 router.get('/logout', userController.logoutAction);
 router.get('/confirm/:token', userController.confirmAction);
+router.post('/event', eventController.addEvent);
 
 module.exports = router;
